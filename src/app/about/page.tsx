651dad23871461f3cc18638a5533319c0db4fbaa
hyeon924 <hyeonn4713@gmail.com>
2024-11-16 17:30:28 +0900

    seconde commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import "@/styles/pages/about.scss"
+
+export default function About() {
+  return (
+    <>
+    <h1>소개페이지</h1>
+    </>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,37 @@
 'use client'; // 클라이언트 컴포넌트로 지정
 
 import React from 'react';
-import { useRouter } from 'next/navigation'; // 'next/navigation'에서 useRouter 사용
+import { usePathname } from 'next/navigation'; // 'next/navigation'에서 usePathname 사용
 import Link from 'next/link';
 import styles from '@/styles/components/header.module.scss';
 
 const Header: React.FC = () => {
-  const router = useRouter();
+  const pathname = usePathname(); // 현재 경로를 확인하기 위해 usePathname 사용
+
+  // 현재 경로가 선택된 링크인지 확인하는 함수
+  const isActive = (path: string) => pathname === path;
 
   return (
     <header className={styles.header}>
-      <Link href="/">홈</Link>
-      {/* 다른 링크나 기능 추가 가능 */}
+      <Link href="/">LOGO</Link>
+
+      <nav className={styles.nav}>
+        <Link href="/about">
+          <span className={isActive('/about') ? styles.active : ''}>토티블록</span>
+        </Link>
+        <Link href="/post">
+          <span className={isActive('/post') ? styles.active : ''}>블로그</span>
+        </Link>
+        <Link href="/qna">
+          <span className={isActive('/qna') ? styles.active : ''}>질문답변</span>
+        </Link>
+        <Link href="/mantor">
+          <span className={isActive('/mantor') ? styles.active : ''}>멘토찾기</span>
+        </Link>
+        <Link href="/mantor/form">
+          <span className={isActive('/mantor/form') ? styles.active : ''}>멘토신청</span>
+        </Link>
+      </nav>
     </header>
   );
 };
